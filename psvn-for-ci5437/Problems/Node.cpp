31fe3018ead0a48c7398a0d15e40afebdda5ba4f
Andrea Salcedo <andreacsalcedo@gmail.com>
2015-05-06 07:31:29 -0430

    Unos cambios al Makefile y principios del UCS con DDD.

--- psvn-for-ci5437/Problems/Node.cpp
+++ psvn-for-ci5437/Problems/Node.cpp
@@ -18,12 +18,13 @@ public:
    string action;
    unsigned cost;
    int depth;
+   int color; // -1 gray, 0 white, 1 black
    
-   Node(state_t s, Node *p, string a, unsigned c, int d) : 
-      state(s), parent(p), action(a), cost(c), depth(d) {};
+   Node(state_t s, Node *p, string a, unsigned c, int d, int cl) : 
+      state(s), parent(p), action(a), cost(c), depth(d), color(cl) {};
       
    Node *makeNode(state_t s, string a) {   
-      return new Node(s,this,a,this->cost,depth+1); // TODO g-cost + costo de funcion 
+      return new Node(s,this,a,this->cost,depth+1, 0); // TODO g-cost + costo de funcion 
    };
    
    void extractPath(vector<string> &reversedPath) {
@@ -38,10 +39,35 @@ public:
    
    static Node *makeRoot(state_t s) {
    
-      return new Node(s,NULL,"",0,0);
+      return new Node(s,NULL,"",0,0,0);
    };
    
-   int getDepth() {
-      return this->depth;
+   void setColor(int color) {
+      this->color = color;
+   };
+
+   int getColor() {
+      return this->color;
+   };
+
+   state_t getState() {
+      return this->state;
+   };
+
+   unsigned getCost() {
+      return this->cost;
+   };
+
+   void setCost(unsigned c) {
+      this->cost = c;
+   };
+};
+
+class CompareCosts {
+public:
+   bool operator()(Node* n1, Node* n2) {
+      if (n1->cost < n2->cost)
+         return false;
+      return true;
    };
 };
---
+++ psvn-for-ci5437/Problems/ucsDDD.cpp
@@ -0,0 +1,123 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <inttypes.h>
+#include <assert.h>
+#include <sys/time.h>
+#include <queue>
+#include <iostream>
+#include "Node.cpp"
+
+#define  MAX_LINE_LENGTH 999 
+
+int main( int argc, char **argv ) {
+// VARIABLES FOR INPUT
+   char str[ MAX_LINE_LENGTH +1 ] ;
+   ssize_t nchars; 
+   state_t state; // state_t is defined by the PSVN API. It is the type used for individual states.
+
+// VARIABLES FOR ITERATING THROUGH state's SUCCESSORS
+   state_t child;
+   ruleid_iterator_t iter; // ruleid_terator_t is the type defined by the PSVN API successor/predecessor iterators.
+   int ruleid ; // an iterator returns a number identifying a rule
+   int childCount = 0;
+    
+   std::queue<state_t> queue;
+   std::queue<int> queue_h;
+// READ A LINE OF INPUT FROM stdin
+   //printf("Please enter a state followed by ENTER: ");
+   //if ( fgets(str, sizeof str, stdin) == NULL ) {
+     // printf("Error: empty input line.\n");
+     // return 0; 
+//   }
+
+// CONVERT THE STRING TO A STATE
+   //nchars = read_state( str, &state );
+   //if (nchars <= 0) {
+   //   printf("Error: invalid state entered.\n");
+   //   return 0; 
+   //}
+   first_goal_state(&state,&ruleid);
+   
+   printf("The state you entered is: ");
+   print_state(stdout, &state);
+   printf("\n");
+   int stateNumber = 0;
+   float branchFactor;
+   //Node *root = Node::makeRoot(state);
+   int hist = init_history;
+
+   queue.push(state);
+   queue_h.push(hist);
+   int depth = 0; 
+   int i, child_hist;
+
+   Node *root = Node::makeRoot(state);
+   
+   priority_queue<Node*, std::vector<Node*>, CompareCosts> pq;
+
+
+   pq.push(root);
+
+   while (!pq.empty()) {
+
+      Node *n = pq.top();
+      pq.pop();
+
+      state_t s = n->getState;
+      
+      if (is_goal(&s))
+         return s;
+
+      hist = s;
+
+        // queue_h.pop();
+      init_fwd_iter(&iter, &s);
+         
+      int dist;
+
+      while((ruleid = next_ruleid(&iter)) >= 0) {
+            
+         apply_fwd_rule(ruleid, &s, &child);
+
+         dist = n->getCost + get_fwd_rule_cost(ruleid);
+
+         if (!n->getColor) {
+
+            n->setColor(-1);
+            n->setCost(dist);
+            n->makeNode(child,get_fwd_rule_label(ruleid));
+         
+         }  else if (dist < n->getCost()) {
+            assert(n->getColor == -1);
+            n->setCost(dist);
+            n->set
+
+         }
+
+
+                  
+            //if (!fwd_rule_valid_for_history(hist,ruleid))
+               //std::cout << "Hist : " << fwd_rule_valid_for_history(hist,ruleid) << "\n";
+             // continue;
+            
+            //hist = childHistory;
+           
+         //printf("child %d. ",childCount);
+         //print_state( stdout, &child );
+
+         //printf("  %s (cost %d), goal=%d\n",get_fwd_rule_label(ruleid),get_fwd_rule_cost(ruleid),is_goal(&child));
+          
+         //Node *newChild = newNode->makeNode(child,get_fwd_rule_label(ruleid));
+            queue.push(child);
+            queue_h.push(next_fwd_history(hist,ruleid));
+         }  
+      }
+
+      // Si no tiene hijos
+      if (childCount == 0) {
+        printf("Your state has no children.\n");
+      }   
+   }
+   return 0;
+   
+} // end main
