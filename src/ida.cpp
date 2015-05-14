b94bc0c3d3ffdff3e236c79d0ddb4d02092f25d0
Andrea Salcedo <andreacsalcedo@gmail.com>
2015-05-14 15:26:24 -0430

    Funciona ida* con un archivo dado.

--- src/ida.cpp
+++ src/ida.cpp
@@ -4,6 +4,9 @@
 #include <assert.h>
 #include <sys/time.h>
 #include <iostream>
+#include <iostream>
+#include <fstream>
+#include <string>
 #include "heuristics.hpp"
 #include <limits>
 
@@ -75,32 +78,13 @@ pair<state_t*,int> boundDFS(state_t *state, int cost, int bound, abstraction_t *
 
 int main(int argc,char **argv) {
 
-    char str[MAX_LINE_LENGTH +1] ;
-    ssize_t nchars; 
-    state_t *state = new state_t; // state_t is defined by the PSVN API. It is the type used for individual states.
-
-    printf("Please enter a state followed by ENTER: ");
-        if (fgets(str, sizeof str, stdin) == NULL) {
-            printf("Error: empty input line.\n");
-            return 0; 
-    }
-
-    nchars = read_state(str,state);
-    
-    if (nchars <= 0) {
-        printf("Error: invalid state entered.\n");
-        return 0; 
-    }
-
-    printf("The state you entered is: ");
-    print_state(stdout,state);
-    printf("\n");
+    state_t *state = new state_t;
 
     FILE *file;
     file = fopen("abs1.state_map","r");
     
     if (file == nullptr) {
-        cout << "Error opening map file.\n";
+        cout << "Error opening map file 1.\n";
         return 0;
     }
     
@@ -109,7 +93,7 @@ int main(int argc,char **argv) {
     file = fopen("abs2.state_map","r");
     
     if (file == nullptr) {
-        cout << "Error opening map file.\n";
+        cout << "Error opening map file 2.\n";
         return 0;
     }
 
@@ -118,13 +102,13 @@ int main(int argc,char **argv) {
     file = fopen("abs3.state_map","r");
     
     if (file == nullptr) {
-        cout << "Error opening map file.\n";
+        cout << "Error opening map file 3.\n";
         return 0;
     }
     
     map3 = read_state_map(file);
     fclose(file);
-
+    
     abstraction_t *abs1 = read_abstraction_from_file("abs1.abst");
   
     if (abs1 == nullptr) {
@@ -136,38 +120,66 @@ int main(int argc,char **argv) {
     abstraction_t *abs3 = read_abstraction_from_file("abs3.abst");
 
     h.initialize(4);
-
-    state_t *stateAbst = new state_t;
-
-    abstract_state(abs1, state, stateAbst);
-    int bound = *state_map_get(map1,stateAbst);
     
-    abstract_state(abs2, state, stateAbst);
-    bound += *state_map_get(map2,stateAbst);
+    string line;
+    ifstream fileStates;
+    fileStates.open ("states.txt");
 
-    abstract_state(abs3, state, stateAbst);
-    bound += *state_map_get(map3,stateAbst);
+    while(!fileStates.eof()) {
 
-    free(stateAbst);
+        getline(fileStates,line);
+
+        if (read_state(line.c_str(),state) <= 0) {
+            printf("Error: invalid state entered.\n");
+            return 0; 
+        }
+
+        printf("The state you entered is: ");
+        print_state(stdout,state);
+        printf("\n");
     
-    //h.getHeuristicNpuzzle(4,state->vars);
+        state_t *stateAbst = new state_t;
 
-    pair<state_t*,int> p;// = new pair;
+        abstract_state(abs1, state, stateAbst);
+        int bound = *state_map_get(map1,stateAbst);
+        
+        abstract_state(abs2, state, stateAbst);
+        bound += *state_map_get(map2,stateAbst);
 
-    while (true) {
-        
-        p = boundDFS(state,0,bound,abs1,abs2,abs3,init_history);
-        
-        if (p.first != nullptr) {
-            cout << "Goal found with cost: " << p.second << endl;
-            return 1;
-        }   
-        bound = p.second;            
+        abstract_state(abs3, state, stateAbst);
+        bound += *state_map_get(map3,stateAbst);
+
+        free(stateAbst);
+    
+        pair<state_t*,int> p;// = new pair;
+
+        while (true) {
+            
+            p = boundDFS(state,0,bound,abs1,abs2,abs3,init_history);
+            
+            if (p.first != nullptr) {
+                cout << "Goal found with cost: " << p.second << endl;
+                break;
+            }   
+            bound = p.second;            
+        }
     }
+    
+    fileStates.close();
 
-    //free(p);
     destroy_abstraction(abs1);
     destroy_abstraction(abs2);
     destroy_abstraction(abs3);
+
+    
+
+
+
+
+
+    //h.getHeuristicNpuzzle(4,state->vars);
+
+
+    //free(p);
 
 }
