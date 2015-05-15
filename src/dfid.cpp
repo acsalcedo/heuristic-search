c70a7db03912d0e247da680d669cafe7c9017408
Andrea Salcedo <andreacsalcedo@gmail.com>
2015-05-15 16:12:01 -0430

    El dfid funciona :')

--- src/dfid.cpp
+++ src/dfid.cpp
@@ -5,77 +5,119 @@
 #include <sys/time.h>
 #include <queue>
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <limits>
-#include <algorithm>    // std::min
+#include <algorithm> 
+#include <time.h>
+
 #define  MAX_LINE_LENGTH 999 
+
+FILE *output;
+unsigned long int nodes = 0;
 
 using namespace std;
 
+pair<state_t*,unsigned long int> boundeddfs(state_t state, unsigned long int bound,unsigned long int depth, int hist) {
+   
+    int ruleid;
+    state_t child;
+    ruleid_iterator_t iter;
 
-// ESTE ESSSS DFSID
-state_t* boundeddfs(state_t state, unsigned int bound, unsigned int depth, int hist) {
-   state_t child, newState;
-   ruleid_iterator_t iter;
-   int ruleid;
-   if (depth > bound) 
-      return nullptr;
+    if (depth > bound) 
+        return make_pair(nullptr,depth);
 
-   if (is_goal(&state))
-      return &state;
+    if (is_goal(&state)) {
+        return make_pair(&state,depth);
+    }
 
-   init_fwd_iter(&iter, &state);
-   while((ruleid = next_ruleid(&iter)) >= 0) {
-      apply_fwd_rule(ruleid, &state, &child);
-      if (!fwd_rule_valid_for_history(hist,ruleid))
-           continue;
-      state_t* n =  boundeddfs(child, depth+1, bound, next_fwd_history(hist,ruleid));
-      if (n != nullptr)
-         return n;
+    init_fwd_iter(&iter,&state);
 
-   }
-   return nullptr;
+    nodes++;
+
+    while((ruleid = next_ruleid(&iter)) >= 0) {
+
+        apply_fwd_rule(ruleid,&state,&child);
+
+        if (!fwd_rule_valid_for_history(hist,ruleid))
+            continue;
+
+        pair<state_t*,unsigned long int> n = boundeddfs(child,bound,depth+1,next_fwd_history(hist,ruleid));
+
+        if (n.first != nullptr)
+            return make_pair(n.first,n.second);
+    }  
+    return make_pair(nullptr,depth);
 }
-
-
-void iterativeddfs(state_t state) {
-   unsigned int bound = 0;
-
-   while (1) {
-      state_t* n = boundeddfs(state, 0, bound, init_history);
-      if (n!= nullptr) {
-         cout << "Goal found" << endl;
-         return;
-      }
-      bound++;
-   }
-}
-
-
 
 int main( int argc, char **argv ) {
-   char str[ MAX_LINE_LENGTH +1 ] ;
-    ssize_t nchars; 
-    state_t state; // state_t is defined by the PSVN API. It is the type used for individual states.
 
-    state_t child;
-    ruleid_iterator_t iter; // ruleid_terator_t is the type defined by the PSVN API successor/predecessor iterators.
-    int ruleid ; // an iterator returns a number identifying a rule
-    int childCount = 0;
+    if (argc < 3) {
+        cout << "Use: ./<exec>.dfid <nameStatesFile> <outputFile>\n";
+        return 0;
+    }
+
+    ifstream fileStates;
+    fileStates.open(argv[1]);
+
+    if (!fileStates.is_open()) {
+        cout << "Error opening file containing states.\n";
+        return 0;
+    }
+
+    output = fopen(argv[2],"w");
     
-    printf("Please enter a state followed by ENTER: ");
-    if ( fgets(str, sizeof str, stdin) == NULL ) {
-        printf("Error: empty input line.\n");
-        return 0; 
+    if (output == nullptr) {
+        cout << "Error opening output file.\n";
+        return 0;
     }
 
-    nchars = read_state( str, &state );
-    if (nchars <= 0) {
-        printf("Error: invalid state entered.\n");
-        return 0; 
+    state_t *state = new state_t; 
+    string line;
+    
+    clock_t t;
+    float secs;
+
+    while(!fileStates.eof()) {
+
+        nodes = 0;
+
+        getline(fileStates,line);
+
+        if (line == "")
+            continue;
+
+        if (read_state(line.c_str(),state) <= 0) {
+            printf("Error: invalid state entered.\n");
+            return 0; 
+        }
+
+        print_state(output,state);
+
+        t = clock();
+        
+        unsigned long int bound = 0;
+
+        pair<state_t*,unsigned long int> n;
+        
+        while (true) {
+          
+            n = boundeddfs(*state,bound,0,init_history);
+
+            if (n.first != nullptr)
+                break;
+
+            bound++;
+        }
+        
+        t = clock() - t;
+    
+        fprintf(output, ": - %lu %lu ", bound, nodes);
+
+        secs = ((float)t)/CLOCKS_PER_SEC;
+
+        fprintf(output,"%f %f\n", secs, nodes/secs);
     }
-   
-    printf("The state you entered is: ");
-    print_state(stdout, &state);
-    printf("\n");
-    iterativeddfs(state);
-} // end main
+
+
+} 
